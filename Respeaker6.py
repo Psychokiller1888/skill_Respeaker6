b3d917313d69c981abad2245aa9f0632fce014c8
Psycho <laurentchervet@bluewin.ch>
2020-08-21 05:55:34 +0200

    Working copy

--- Respeaker6.py
+++ Respeaker6.py
@@ -1,6 +1,6 @@
+import RPi.GPIO as GPIO
+
 from core.base.model.AliceSkill import AliceSkill
-from core.dialog.model.DialogSession import DialogSession
-from core.util.Decorators import IntentHandler
 
 
 class Respeaker6(AliceSkill):
@@ -9,6 +9,43 @@ class Respeaker6(AliceSkill):
 	Description: Let's you configure the gpio button available on the device
 	"""
 
-	@IntentHandler('MyIntentName')
-	def dummyIntent(self, session: DialogSession, **_kwargs):
-		pass
+	GPIO_PIN = 26
+
+	def __init__(self):
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+		GPIO.setup(self.GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+		super().__init__()
+
+
+	def onStart(self):
+		super().onStart()
+		GPIO.add_event_detect(self.GPIO_PIN, GPIO.FALLING, callback=self.onButton, bouncetime=300)
+
+
+	def onStop(self):
+		super().onStop()
+		GPIO.remove_event_detect(self.GPIO_PIN)
+
+
+	def onButton(self, _channel: int):
+		action = self.getConfig('buttonAction')
+
+		try:
+			method = getattr(self, action)
+			method()
+		except:
+			self.logWarning(f'Button press detected but method **{action}** does not exist')
+
+
+	def toggleMute(self):
+		self.WakewordManager.toggleEngine()
+
+
+	def startListening(self):
+		if not self.getAliceConfig('deviceName') in self.DialogManager.sessionsBySites:
+			self.DialogManager.onHotword(self.getAliceConfig('deviceName'))
+
+
+	def stopSpeaking(self):
+		self.AudioServer.stopPlaying()
